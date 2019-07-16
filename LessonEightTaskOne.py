95c8937d9522a4e46b4f873dd756b6c05f590029
esergly <kievbsm@gmail.com>
2019-07-16 18:39:03 +0300

    The first commit to check

--- LessonEightTaskOne.py
+++ LessonEightTaskOne.py
@@ -3,3 +3,14 @@ def calculate_sum(a, b):
 
 
 print(type(calculate_sum))
+
+a = int(input())
+b = int(input())
+
+
+def main():
+    print(int(calculate_sum(a, b)))
+
+
+if __name__ == '__main__':
+    main()
